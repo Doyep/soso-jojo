f7bf0744879b100f7b67ebe1b9b520e6f507315d
Joseph NGUYEN <joseph.nguyen.90@outlook.com>
2024-01-13 17:03:50 +0100

    feat: create mobile and desktop layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
-  { path: '', component: FormComponent },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [{
+      path: '',
+      component: FormComponent
+    }]
+  },
   { path: '**', redirectTo: '' },
 ];
