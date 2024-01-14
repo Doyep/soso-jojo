3d731ebbe8be257c743e01b42785d8de5c399532
Joseph NGUYEN <joseph.nguyen.90@outlook.com>
2024-01-14 17:50:13 +0100

    wip: feat: create login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 import { FormComponent } from './components/form/form.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { AnnouncementComponent } from './components/announcement/announcement.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
     children: [
-      { path: '', redirectTo: 'announcement', pathMatch: 'full' },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
       { path: 'announcement', component: AnnouncementComponent },
       { path: 'form', component: FormComponent }
     ]
