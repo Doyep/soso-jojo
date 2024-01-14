34b67c37d2094e8a71dc29a94c4fc3edf69c9c6e
Joseph NGUYEN <joseph.nguyen.90@outlook.com>
2024-01-14 16:10:23 +0100

    wip: add return button

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -5,18 +5,26 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, GuestFormComponent, MatExpansionModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, GuestFormComponent, MatExpansionModule, MatButtonModule, MatIconModule, HttpClientModule],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+  styleUrl: './form.component.scss',
+  providers: [HttpClientModule]
 })
 export class FormComponent {
   public form: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router,
+  ) {
     this.form = this.formBuilder.group({
       guests: this.formBuilder.array([
         this.createGuestFormGroup()
@@ -43,5 +51,14 @@ export class FormComponent {
   public onRemoveGuest(index: number) {
     if (this.guests.controls.length === 1) return
     this.guests.removeAt(index)
+  }
+
+  public onSubmit() {
+    const path = environment.baseUrl + '/guest'
+    this.http.post<any>(path, this.guests.value).subscribe(response => console.log(response))
+  }
+
+  public navigateToAnnouncement() {
+    this.router.navigateByUrl('/announcement')
   }
 }
