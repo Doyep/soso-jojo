9403aa6263bcd8635f23e4db812a733fd3edebc2
Joseph NGUYEN <joseph.nguyen.90@outlook.com>
2024-01-18 07:58:35 +0100

    refactor (announcement): update code structure, wording, style

--- src/app/pages/announcement/announcement.component.ts
+++ src/app/pages/announcement/announcement.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { Router, RouterModule } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-announcement',
   standalone: true,
-  imports: [RouterModule, MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './announcement.component.html',
   styleUrl: './announcement.component.scss'
 })
 export class AnnouncementComponent {
-  constructor(private router: Router) { }
-
-  public navigateToList() {
-    this.router.navigateByUrl('/list')
-  }
 }
